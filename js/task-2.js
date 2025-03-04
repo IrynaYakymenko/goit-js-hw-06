940b2eae87f46842400c84564d0d25fbf068df1c
IrynaYakymenko <irynayakymenko93@gmail.com>
2025-03-04 15:41:04 +0200

    all

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,21 @@
 class Storage {
+    #items
     constructor(items){
-        this.items = items;
+        this.#items = items;
     }
     
     getItems(){
-        return this.items;
+        return this.#items;
     }
 
     addItem(newItem){
-        this.items.push(newItem);
+        this.#items.push(newItem);
     }
 
     removeItem(itemToRemove){
-        if (this.items != itemToRemove){
-            this.items = this.items.filter(el => el !== itemToRemove);
-        }
+        if (this.#items.indexOf(itemToRemove) !== -1){
+            this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+        }       
     }
 }
 
